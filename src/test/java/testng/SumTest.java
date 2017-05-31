6695adaefa968a677c0747ae8b178611ca6176c8
Volha Karzhova <olga1584624@gmail.com>
2017-05-31 21:26:11 +0300

    add  suite.xml

--- src/test/java/testng/SumTest.java
+++ src/test/java/testng/SumTest.java
@@ -2,15 +2,17 @@ package testng;
 
 import com.epam.tat.module4.Calculator;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class SumTest {
 
    @Test
-    public void testSum () {
+   @Parameters({"a", "b", "expected"})
+    public void testSum (long a, long b, long expected) {
         Calculator calculator = new Calculator();
-        long sum = calculator.sum(1,1);
-        Assert.assertEquals(sum, 2);
+        long sum = calculator.sum(a, b);
+        Assert.assertEquals(sum, expected);
 
     }
 }
